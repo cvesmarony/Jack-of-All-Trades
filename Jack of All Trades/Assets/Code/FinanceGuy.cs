bbf00256773ff20d9d6798cbaa17e5187dabcb75
andriachen <andriachen.n@gmail.com>
2025-02-16 14:16:26 -0500

    Added score
    updates score everytime player picks up a file

--- Jack of All Trades/Assets/Code/FinanceGuy.cs
+++ Jack of All Trades/Assets/Code/FinanceGuy.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FinanceGuy : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float moveSpeed = 2f;
     public Vector2 movement;
+
+    private int score = 0; // Track the number of files collected
+    public Text scoreText; // Reference to the Score UI Text
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        UpdateScoreUI(); // Initialize score display
     }
 
     // Listen for player input to move the object: 
@@ -25,6 +31,20 @@ public class FinanceGuy : MonoBehaviour
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 
+    public void AddScore(int points)
+    {
+        score += points;  // Increase the score by the points passed
+        UpdateScoreUI(); // Update the UI
+    }
+
+    void UpdateScoreUI()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + score;
+        }
+    }
+    
      // Makes objects with the tag "files" disappear on contact
      // can change files to be tasks later!
     void OnCollisionEnter2D(Collision2D other){
@@ -33,3 +53,4 @@ public class FinanceGuy : MonoBehaviour
         }
     }
 }
+
--- Jack of All Trades/Assets/Code/fileCollect.cs
+++ Jack of All Trades/Assets/Code/fileCollect.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class fileCollect : MonoBehaviour
 {
+    private bool isCollected = false; // To prevent multiple triggers on the same object
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) // Ensure the Player is tagged correctly
+        if (!isCollected && other.CompareTag("Player")) 
         {
-            Destroy(gameObject); // remove the item when touched
+            isCollected = true; 
+
+            FinanceGuy playerScript = other.GetComponent<FinanceGuy>();
+            if (playerScript != null)
+            {
+                playerScript.AddScore(1); // Increase score by 1
+            }
+
+            Destroy(gameObject); 
         }
     }
 }
