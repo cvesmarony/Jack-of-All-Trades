663452f104e53f76b817fcdfe455d8ce4333df9d
Susan <susan4tnt@gmail.com>
2025-02-23 08:21:16 -0500

    Added player health and cubicle boundaries
    Changed the player rigidbody to dynamic with gravity set to 0 and rotation on the z axis off. Cubicle has a polygon collider with a kinematic rigidbody. Added a capsule collider to the light with istrigger on. Player health will go down every 0.1 seconds if colliding with the light. The capsule collider moves with the light.

--- Jack of All Trades/Assets/Code/FinanceGuy.cs
+++ Jack of All Trades/Assets/Code/FinanceGuy.cs
@@ -3,39 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class FinanceGuy : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float moveSpeed = 2f;
-    public Vector2 movement;
+    private Vector2 movement;
+
 
     private int score = 0; // Track the number of files collected
     public Text scoreText; // Reference to the Score UI Text
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0; // Disable gravity
+        rb.freezeRotation = true; // Prevent unwanted rotation
         UpdateScoreUI(); // Initialize score display
     }
 
-    // Listen for player input to move the object: 
+
+    void Update()
+    {
+        // Get movement input
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+
+        movement = new Vector2(moveX, moveY).normalized; // Normalize to prevent diagonal speed boost
+    }
+
+
     void FixedUpdate()
     {
-        // if you move diagonally (2 inputs) you will go faster
-        // can change this to be the same speed somehow...
-        movement.x = Input.GetAxisRaw ("Horizontal");
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-        
-        movement.y = Input.GetAxisRaw ("Vertical");
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        // Apply velocity for movement
+        rb.velocity = movement * moveSpeed;
     }
+
 
     public void AddScore(int points)
     {
-        score += points;  // Increase the score by the points passed
+        score += points;  // Increase the score
         UpdateScoreUI(); // Update the UI
     }
+
 
     void UpdateScoreUI()
     {
@@ -44,13 +57,14 @@ public class FinanceGuy : MonoBehaviour
             scoreText.text = "Score: " + score;
         }
     }
-    
-     // Makes objects with the tag "files" disappear on contact
-     // can change files to be tasks later!
-    void OnCollisionEnter2D(Collision2D other){
-        if (other.gameObject.tag == "files"){
+
+
+    // Makes objects with the tag "files" disappear on contact
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("files"))
+        {
             Destroy(other.gameObject);
         }
     }
 }
-
