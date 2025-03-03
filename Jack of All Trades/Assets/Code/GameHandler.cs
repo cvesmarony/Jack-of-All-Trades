cab09484233e914f96ef5b4b3f7fbddb2a8cd4d2
Susan <susan4tnt@gmail.com>
2025-03-03 17:58:59 -0500

    Added paused menu
    there is an issue with clicking the buttons

--- Jack of All Trades/Assets/Code/GameHandler.cs
+++ Jack of All Trades/Assets/Code/GameHandler.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class GameHandler : MonoBehaviour
@@ -14,6 +15,7 @@ public class GameHandler : MonoBehaviour
     public GameObject DataText; // Display for data collection
     public GameObject HealthText; // Display for health UI
     public static bool stairCaseUnlocked = false;
+    private string sceneName;
 
     void Start()
     {
@@ -33,10 +35,12 @@ public class GameHandler : MonoBehaviour
         {
             Debug.LogError("DataText UI not assigned in the Inspector!");
         }
-
+        sceneName = SceneManager.GetActiveScene().name;
         currHealth = maxHealth;
         updateStatsDisplay();
     }
+
+
 
     public void updateStatsDisplay()
     {
@@ -70,9 +74,42 @@ public class GameHandler : MonoBehaviour
         }
     }
 
+    void Update(){
+               
+    } 
+
     public void gameOver()
     {
         Debug.Log("Game Over! Restarting...");
+        // SceneManager.LoadScene("GameOver");
+        StartCoroutine(DeathPause());
+    }
+
+    IEnumerator DeathPause(){
+        yield return new WaitForSeconds(1.0f);
         SceneManager.LoadScene("GameOver");
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene("TUTORIAL_1");
+    }
+
+    public void RestartGame() {
+        Time.timeScale = 1f;
+        GameHandler_PauseMenu.GameisPaused = false;
+        SceneManager.LoadScene("MainMenu");
+        // playerHealth = StartPlayerHealth;
+    }
+
+    public void Credits() {
+        SceneManager.LoadScene("CreditsMenu");
+    }
+
+    public void QuitGame() {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
     }
 }
