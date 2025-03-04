f81abaa6189abe4c02758c2767c340a3bb17c8b1
tasnim13 <tasnim.hossain@tufts.edu>
2025-03-03 21:04:52 -0500

    working hand off!!!!!!

--- Jack of All Trades/Assets/Code/ComputerInteraction.cs
+++ Jack of All Trades/Assets/Code/ComputerInteraction.cs
@@ -13,7 +13,7 @@ public class ComputerInteraction : MonoBehaviour
     private float taskDuration = 3f;
     private float taskProgress = 0f;
     private bool isInteracting = false;
-    private int score = 0;
+    private int data = 0;
     private GameObject currentComputer;
     private HashSet<GameObject> completedComputers = new HashSet<GameObject>();
 
@@ -74,13 +74,46 @@ public class ComputerInteraction : MonoBehaviour
         }
     }
 
-    void CompleteTask()
+//     void CompleteTask()
+// {
+//     if (currentComputer != null)
+//     {
+//         // add score
+//         data++;
+//         scoreText.text = "Data: " + data;
+
+//         // Get the child objects with the sprite renderers
+//         Transform incompleteSprite = currentComputer.transform.Find("IncompleteSprite");
+//         Transform completedSprite = currentComputer.transform.Find("CompletedSprite");
+
+//         // If both sprites are found, switch them
+//         if (incompleteSprite != null) 
+//             incompleteSprite.gameObject.SetActive(false); // Hide incomplete sprite
+//         if (completedSprite != null) 
+//             completedSprite.gameObject.SetActive(true); // Show completed sprite
+
+//         // Mark the current computer as completed, so it cant be interacted with again
+//         completedComputers.Add(currentComputer);
+
+//         // Disable collider so player can't trigger it again
+//         currentComputer.GetComponent<Collider2D>().enabled = false; 
+
+//         // Reset interaction
+//         taskProgress = 0f;
+//         taskProgressSlider.value = 0f;
+//         isInteracting = false;
+//         currentComputer = null;
+//     }
+// }
+
+
+void CompleteTask()
 {
     if (currentComputer != null)
     {
-        // add score
-        score++;
-        scoreText.text = "Data: " + score;
+        // Add to global data count in GameHandler
+        GameHandler.gotData++;
+        GameHandler.instance.updateStatsDisplay(); // Update UI from GameHandler
 
         // Get the child objects with the sprite renderers
         Transform incompleteSprite = currentComputer.transform.Find("IncompleteSprite");
@@ -92,7 +125,7 @@ public class ComputerInteraction : MonoBehaviour
         if (completedSprite != null) 
             completedSprite.gameObject.SetActive(true); // Show completed sprite
 
-        // Mark the current computer as completed, so it cant be interacted with again
+        // Mark the current computer as completed, so it can't be interacted with again
         completedComputers.Add(currentComputer);
 
         // Disable collider so player can't trigger it again
@@ -105,5 +138,6 @@ public class ComputerInteraction : MonoBehaviour
         currentComputer = null;
     }
 }
+
 
 }
